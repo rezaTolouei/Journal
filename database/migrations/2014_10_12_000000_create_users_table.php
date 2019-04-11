5318401af7e230a5a2e46a0f9e0d4689d7c7b7a2
rezaTolouei <xcalibo@gmail.com>
2019-04-12 02:25:06 +0430

    Make auth and make AdminUserController

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,22 +16,22 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name');
-            $table->string('last_name');
+          //  $table->string('last_name');
             $table->string('email')->unique();
             $table->string('password');
-            $table->string('salutation');
-            $table->string('contact_type');
-            $table->string('degree');
-            $table->string('discipline');
-            $table->string('url');
-            $table->string('mobile');
-            $table->string('phone');
-            $table->string('fax');
-            $table->unsignedInteger('province_id');
-            $table->string('city');
-            $table->string('mailing_address');
-            $table->tinyInteger('disabled')->default('0'); //tiny int
-            $table->text('disabled_reason');
+          //  $table->string('salutation');
+          //  $table->string('contact_type');
+          //  $table->string('degree');
+          //  $table->string('discipline');
+          //  $table->string('url');
+          //  $table->string('mobile');
+          //  $table->string('phone');
+          //  $table->string('fax');
+          //  $table->unsignedInteger('province_id');
+          //  $table->string('city');
+          //  $table->string('mailing_address');
+          //  $table->tinyInteger('disabled')->default('0'); //tiny int
+          //  $table->text('disabled_reason');
             $table->timestamp('email_verified_at')->nullable();
             $table->rememberToken();
             $table->timestamps();
